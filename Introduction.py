63b9b26fb8640dd940d98ccc4a1f8d430c8554c3
jcarpanini <jcarpanini@albany.edu>
2025-10-05 20:09:27 -0400

    Update Introduction.py
    added comments and if else statement for collaboration. JC

--- Introduction.py
+++ Introduction.py
@@ -1,4 +1,11 @@
 # This program is an introductory program for this repository
 
 name = input("Hello! What is your name? ")
-print("Your name is:", name)
+#print("Your name is:", name)
+
+#collaboration JC for assignment 405/505
+# Make sure the user typed something
+if name.strip() == "":
+    print("You didn't enter a name.")
+else:
+    print("Nice to meet you,", name + "!")
